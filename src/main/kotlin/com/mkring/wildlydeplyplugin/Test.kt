e53e4d2b66b8b27a9816436f66a5235e1ec99a41
Marcel Kesselring <Ingwersaft@users.noreply.github.com>
2017-12-06 16:17:01 +0100

    added cli connect proof of concept

--- src/main/kotlin/com/mkring/wildlydeplyplugin/Test.kt
+++ src/main/kotlin/com/mkring/wildlydeplyplugin/Test.kt
@@ -1,5 +1,20 @@
 package com.mkring.wildlydeplyplugin
 
+import org.jboss.`as`.cli.scriptsupport.CLI
+import java.io.File
+
 fun main(args: Array<String>) {
     println("hello")
+    val file = File("/home/mauer/workspace/uploadreceiver/build/libs/UploadReceiver-1.0.0-SNAPSHOT.war")
+    println(file.exists())
+
+    val cli = CLI.newInstance()
+    cli.connect("localhost", 10090, "jboss", "root1234".toCharArray())
+    val result = cli.cmd(":read-attribute(name=server-state) ")
+    val response = result.getResponse()
+    val serverstate = response.get("result")
+
+    println("Current server state: " + serverstate)
+
+    cli.disconnect()
 }
