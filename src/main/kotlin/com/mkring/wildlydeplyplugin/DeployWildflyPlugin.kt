d97b5dc6be11fa08ad427c3ccaaf7672c98d3845
Marcel Kesselring <Ingwersaft@users.noreply.github.com>
2017-12-07 13:35:48 +0100

    first draft of plugin. works in main() at least

--- src/main/kotlin/com/mkring/wildlydeplyplugin/DeployWildflyPlugin.kt
+++ src/main/kotlin/com/mkring/wildlydeplyplugin/DeployWildflyPlugin.kt
@@ -1,20 +1,53 @@
 package com.mkring.wildlydeplyplugin
 
+import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.tasks.Copy
-import org.gradle.kotlin.dsl.invoke
+import org.gradle.api.tasks.TaskAction
+import java.io.File
 
 open class DeployWildflyPlugin : Plugin<Project> {
     override fun apply(project: Project) {
-        project.run {
-            tasks {
-                "myCopyTask"(Copy::class) {
-                    from("build.gradle.kts")
-                    into("build/copy")
-                }
-            }
+        with(project) {
+            configurations.maybeCreate("deployWildfly")
+                    .setVisible(false).description = "Deploy Wildfly Configuration"
+
+            extensions.create("deployWildfly", DeployWildplyPluginExtension::class.java)
+            tasks.create("deployWildfly", DeployWildflyTask::class.java)
         }
     }
 
+}
+
+open class DeployWildplyPluginExtension {
+    var file: File? = null
+    var host: String = "localhost"
+    var port: Int = 9090
+    var user: String? = null
+    var password: String? = null
+    var reload: Boolean = true
+    var force: Boolean = true
+}
+
+open class DeployWildflyTask : DefaultTask() {
+
+    init {
+        group = "help"
+        description = "Deploys files to a Wildfly und reloads it afterwards"
+    }
+
+    @TaskAction
+    fun deployWildfly() {
+        with(project) {
+            val extension = extensions.getByName("deployWildfly") as DeployWildplyPluginExtension
+            if (extension.file == null || extension.user == null || extension.password == null) {
+                println("missing configuration")
+                return
+            }
+            with(extension) {
+                println("deployWildfly: going to deploy ${file?.absolutePath} to $host:$port")
+            }
+            FileDeployer(extension).deploy()
+        }
+    }
 }
