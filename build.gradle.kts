fa98326d877144463afd9117a7efda7702f75211
Marcel Kesselring <Ingwersaft@users.noreply.github.com>
2017-12-07 14:13:55 +0100

    added publishing to plugin repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,16 @@
+import org.gradle.kotlin.dsl.`kotlin-dsl`
 import org.gradle.kotlin.dsl.kotlin
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "com.mkring.wildlydeplyplugin"
-version = "1.0.0"
+version = "0.1"
 
 plugins {
     kotlin("jvm") version "1.2.0"
     `java-gradle-plugin`
     `kotlin-dsl`
     `maven-publish`
+    id("com.gradle.plugin-publish").version("0.9.9")
 }
 
 repositories {
@@ -19,8 +21,22 @@ repositories {
 gradlePlugin {
     (plugins) {
         "deploy-wildfly-plugin" {
-            id = "deploy-wildfly-plugin"
+            id = "com.mkring.wildlydeplyplugin.deploy-wildfly-plugin"
             implementationClass = "com.mkring.wildlydeplyplugin.DeployWildflyPlugin"
+        }
+    }
+}
+
+pluginBundle {
+    website = "http://www.gradle.org/"
+    vcsUrl = "https://github.com/Ingwersaft/wildfly-deploy-gradle-plugin"
+    description = "Deploys files to a Wildfly und reloads it afterwards"
+    tags = listOf("deploy", "wildfly", "jboss-as-cli")
+
+    (plugins){
+        "deploy-wildfly-plugin" {
+            id = "com.mkring.wildlydeplyplugin.deploy-wildfly-plugin"
+            displayName = "Wildfly Deploy Gradle Plugin"
         }
     }
 }
