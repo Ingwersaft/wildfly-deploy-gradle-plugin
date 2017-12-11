5e37ff659b42f75ad2dc43c4e7d8d5054ec0dd4e
Marcel Kesselring <Ingwersaft@users.noreply.github.com>
2017-12-11 07:35:54 +0100

    removed default plugin task and made DeployWildflyTask configurable

--- src/main/kotlin/com/mkring/wildlydeplyplugin/Test.kt
+++ src/main/kotlin/com/mkring/wildlydeplyplugin/Test.kt
@@ -2,12 +2,11 @@ package com.mkring.wildlydeplyplugin
 
 fun main(args: Array<String>) {
     println("hello")
-    val extension = DeployWildplyPluginExtension().apply {
-        host = "host"
-        port = 9090
-        user = "user"
-        password = "pass"
-        file = "filePath"
-    }
-    FileDeployer(extension).deploy()
+    DeployWildflyTask(
+            host = "host",
+            port = 9090,
+            user = "user",
+            password = "pass",
+            file = "filePath"
+    ).deployWildfly()
 }
