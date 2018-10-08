bd7877f8e31170cde4d32af2f3b946ec5f4b01f8
Marcel Kesselring <marcel.kesselring@1und1.de>
2018-10-08 13:11:23 +0200

    manually adapted to version 0.2.7 as plugin release preparation

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jetbrains.kotlin.serialization.js.DynamicTypeDeserializer.id
 
 group = "com.mkring.wildlydeplyplugin"
-version = "0.2.7-SNAPSHOT"
+version = "0.2.7"
 
 plugins {
     kotlin("jvm") version "1.2.61"
