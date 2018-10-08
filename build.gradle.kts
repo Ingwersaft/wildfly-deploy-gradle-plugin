bafe051e6b86ceeb34886ca6896c15691e012b52
Marcel Kesselring <marcel.kesselring@1und1.de>
2018-10-08 12:16:27 +0200

    enable newly deployed deployment if it is disabled implementing #3

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jetbrains.kotlin.serialization.js.DynamicTypeDeserializer.id
 
 group = "com.mkring.wildlydeplyplugin"
-version = "0.2.6"
+version = "0.2.7-SNAPSHOT"
 
 plugins {
     kotlin("jvm") version "1.2.61"
