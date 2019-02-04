1effd23f6a4c6373ae97b1f838cb576bd40a1771
Ingwersaft <Ingwersaft@users.noreply.github.com>
2019-02-04 16:20:05 +0100

    created wildfly15 branch and adapted jboss cli jar dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jetbrains.kotlin.serialization.js.DynamicTypeDeserializer.id
 
 group = "com.mkring.wildlydeplyplugin"
-version = "0.2.10"
+version = "0.2.10-wildfly15"
 
 plugins {
     kotlin("jvm") version "1.2.61"
@@ -42,7 +42,7 @@ pluginBundle {
 dependencies {
     compile(kotlin("stdlib-jdk8", kotlinVersion))
     runtime(kotlin("reflect", kotlinVersion))
-    compile("org.wildfly", "wildfly-cli", "8.2.1.Final")
+    compile("org.wildfly.core", "wildfly-cli", "7.0.0.Final")
 
     compileOnly(gradleApi())
 
