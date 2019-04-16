20b22089cf72ed4253e7f58a4793448de7625ef4
Ingwersaft <Ingwersaft@users.noreply.github.com>
2019-04-16 20:06:20 +0200

    basic circleci build integration (#19)
    * added basic circleci build
    
    * added wildfly job
    
    * fixed wildfly steps
    
    * fixed wildfly steps (v2)
    
    * added wildfly build dependency
    
    * fixed build job
    
    * fixed build job (v2)
    
    * removed all caching
    
    * added x flag to gradlew
    
    * adapted wildfly command
    
    * added ps aux step
    
    * fixed ps aux
    
    * added nmap
    
    * changed nmap params
    
    * added sleeps; gradle deploy; curl
    
    * start wildfly image manually
    
    * adapted to use circleci machine
    
    * added mgmnt user creation
    
    * added 8080 mapping
    
    * added verify task

--- integration/build.gradle.kts
+++ integration/build.gradle.kts
@@ -1,4 +1,7 @@
 import com.mkring.wildlydeplyplugin.DeployWildflyTask
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.HttpClients
+import org.apache.http.util.EntityUtils
 import org.gradle.internal.id.UUIDGenerator
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
@@ -6,6 +9,14 @@ plugins {
     kotlin("jvm") version "1.3.21"
     id("com.mkring.wildlydeplyplugin.deploy-wildfly-plugin") version "0.2.10"
     war
+}
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.apache.httpcomponents:httpclient:4.5.8")
+    }
 }
 
 group = "com.mkring.wildlydeplyplugin"
@@ -24,18 +35,30 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
-
-task("build-id") {
-    doFirst {
-        File("build.id").apply {
-            createNewFile()
-            val id = UUIDGenerator().generateId().toString()
-            println("buildId=$id")
-            writeText(id)
+task("verify") {
+    doLast {
+        val fileId = File("build.id").readText()
+        val apiId =
+            HttpClients.createDefault().execute(HttpGet("http://localhost:8080/integration-1/rest/id")).entity.let {
+                EntityUtils.toString(it)
+            }
+        println("fileId=$fileId")
+        println("apiId=$apiId")
+        if (!fileId.equals(apiId)) {
+            throw RuntimeException("fileId.equals(apiId) == false!")
         }
+    }
+}
+task("build-id") {
+    File("build.id").apply {
+        createNewFile()
+        val id = UUIDGenerator().generateId().toString()
+        println("buildId=$id")
+        writeText(id)
     }
     outputs.doNotCacheIf("always generate new id") { true }
 }
+tasks.findByPath("build")?.outputs?.cacheIf { false }
 task("deploy", DeployWildflyTask::class) {
     host = "localhost"
     port = 9990
