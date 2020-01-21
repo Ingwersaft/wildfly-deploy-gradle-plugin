358db183414b70e5644f22bc23ddedb30c63bc23
Ingwersaft <ingwersaft@users.noreply.github.com>
2020-01-21 19:30:33 +0100

    feat(versioning): increase version to 0.3.0-wildfly after recent API changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jetbrains.kotlin.serialization.js.DynamicTypeDeserializer.id
 
 group = "com.mkring.wildlydeplyplugin"
-version = "0.2.10-wildfly15"
+version = "0.3.0-wildfly15"
 
 plugins {
     kotlin("jvm") version "1.2.61"
